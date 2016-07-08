d70fff5795a74342adcdfdcf1ec4310cfdf8f99d
dnivedha <nivethadurairaju95@gmail.com>
2016-07-08 11:51:37 +0530

    finish

---
+++ README.c
@@ -0,0 +1,15 @@
+# ni
+#include<stdio.h>
+include<conio.h>
+void main()
+{
+int a;
+printf("enter the no");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("the no is even",n);
+else
+printf("the no is odd",n);
+}
+}
